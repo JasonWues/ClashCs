3ff8543058cf74e90899d013c61d73c0fcfae7a8
Metatron <panruzun@gmail.com>
2023-04-14 15:02:12 +0800

    config check

---
+++ ClashCs.Server/Config/GlobalConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace ClashCs.Server.Config;
+
+public class GlobalConfig
+{
+    public Model.Config StartConfig = new Model.Config();
+    public List<Model.Config> Configs = new List<Model.Config>();
+}
